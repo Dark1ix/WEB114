c080becbe35d35073fb4e21024196ab225bc1c82
Somto <bbb10gamer@gmail.com>
2025-03-12 01:01:37 -0400

    Update budget.js

--- budget.js
+++ budget.js
@@ -11,8 +11,7 @@ let RentP = Math.max((rent / income) * 100,0)
 let GroceriesP = Math.max((groceries / income) * 100,0)
 
 console.log( "The total income was: " +   income.toLocaleString("en-US", {style: "currency", currency: "USD", }));
-console.log( "The grocery total was: " +  groceries.toLocaleString("en-US", { style: "currency", currency:"USD" }));
+console.log( "The grocery total was: " +  groceries.toLocaleString("en-US", { style: "currency", currency: "USD" }));
 console.log( "The rent amount was: " +   rent.toLocaleString("en-US", {style: "currency", currency: "USD", }));
-console.log( "The percent spent on rent was: " +  rentP + "%"}));
-console.log( "The percent spent on groceries was: " +  GroceriesP + "%"}));
-
+console.log( "The percent spent on rent was: " +  rentP + "%");
+console.log( "The percent spent on groceries was: " +  GroceriesP + "%");

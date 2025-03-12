f533f13918f2231a9d1f5d8bb4e7c488b38674c2
Somto <bbb10gamer@gmail.com>
2025-03-11 21:39:46 -0400

    Add files via upload

---
+++ budget.js
@@ -0,0 +1,13 @@
+let income = 2500
+let rent = 1000
+let groceries = 300
+let utilities = 500
+
+let totalExpenses = rent + groceries + utilities
+let remainingMoney = totalExpenses - income
+
+let RentP = Math.max((rent / income) * 100,0)
+let GroceriesP = Math.max((groceries / income) * 100,0)
+
+console.log(rentP)
+console.log(groceiresP)

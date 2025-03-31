53ce59dd510cc193b439430f6b52dca0ef5d3757
Somto <bbb10gamer@gmail.com>
2025-03-30 23:40:18 -0400

    Update week 3.js

--- week 3.js
+++ week 3.js
@@ -1,3 +1,17 @@
+<!DOCTYPE HTML>
 <html>
-  <--Somtochukwu Onyenagubo-->
-  
+<body>
+<script>
+let icecream = prompt("What's your favorite ice cream flavor?");
+console.log("Your favorite ice cream flavor is", icecream);
+if (icecream == "Vanilla") {
+    console.log("You have basic taste and that's okay!");
+    console.log("Vanilla is also one of my favorites.");
+} else if (icecream == "Chocolate") {
+    console.log("Choco is a really nice thick flavor.");
+    console.log("It makes my stomach hurt when I have too much");
+} else if (icecream == "Strawberry") {
+    console.log("Strawberry's a sweet soft flavor.");
+    console.log("I don't know about you but I enjoy mine with small strawberry pieces.");
+</body>
+</html>

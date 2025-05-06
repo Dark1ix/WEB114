4658f595e6e9bd57b16fe58442199832a1e41f78
Somto <bbb10gamer@gmail.com>
2025-05-06 15:16:12 -0400

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,9 +1,24 @@
-document.getElementById("taskInput");
-document.getElementById("tasklist");
-document.getElementById("addTaskBtn").addEventListener('click',hello);
-function hello() {
-    if (taskInput == "hey")
-       echo ('hi');
-    else
-        ("okay");
-};
+const text = document.getElementById("taskInput");
+const list = document.getElementById("tasklist");
+const button = document.getElementById("addTaskBtn")
+
+button.addEventListener("click", function() {
+    const taskInput = text.value.trim();
+
+    if (taskInput === "") {
+      alert("You can not enter nothing.");
+      return; }
+    const itemlist = document.createElement("li")
+    itemlist.textContent = taskInput;
+
+    const remove = document.createElement("button");
+    remove.textContent = "Delete";
+
+    remove.addEventListener("click", function () {
+          list.removeChild(itemlist);
+        });
+    itemlist.appendChild(remove);
+    list.appendChild(itemlist);
+    text.value = "";
+    text.focus();
+});

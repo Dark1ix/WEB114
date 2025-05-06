703578536817e6f3631bde454cee5600bdaa4bc5
Somto <bbb10gamer@gmail.com>
2025-05-06 05:47:44 -0400

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,9 @@
+document.getElementById("taskInput");
+document.getElementById("tasklist");
+document.getElementById("addTaskBtn").addEventListener('click',hello);
+function hello() {
+    if (taskInput == "hey")
+       echo ('hi');
+    else
+        ("okay");
+};
